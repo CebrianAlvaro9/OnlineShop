0bcf06bb68f8f55ea0cceadf46dca4684bb3337d
Alvaro Cebrian 2A <113922067+CebrianAlvaro9@users.noreply.github.com>
2024-08-19 13:11:35 +0200

    project set up

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { ThemeSwitcher } from "./componets/darkmode/ThemeSwitcher";
+
+function App() {
+  return (
+    <>
+      <header className="p-5 flex flex-row items-center justify-center gap-12">
+        <h1 className="text-4xl font-bold dark:text-slate-50 ">Online Shop</h1>
+        <div>
+          <ThemeSwitcher />
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default App;
---
+++ src/componets/darkmode/ThemeSwitcher.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import { MoonIcon } from "./MoonIcon";
+import { SunIcon } from "./SunIcon";
+
+export const ThemeSwitcher = () => {
+  const [darkMode, setDarkMode] = useState(true);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.querySelector("html")?.classList.add("dark");
+    } else {
+      document.querySelector("html")?.classList.remove("dark");
+    }
+  }, [darkMode]);
+
+  return (
+    <span
+      onClick={() => setDarkMode(!darkMode)}
+      className="w-8 h-8
+              flex items-center justify-center
+              rounded-lg bg-default-100  hover:bg-default-200 border "
+    >
+      {darkMode ? <SunIcon /> : <MoonIcon />}
+    </span>
+  );
+};
